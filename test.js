ce44bd16956f57fb93be87906666a08452556011
Apoorva Saxena <apsaxena24@gmail.com>
2017-08-01 01:42:59 +0100

    Readme fixed

---
+++ test.js
@@ -0,0 +1,55 @@
+// const request = require('request')
+
+
+// async function getWorldData() {
+//     return new Promise(function(resolve, reject) {
+//         request('https://restcountries.eu/rest/v2/all', function(error, response, body) {
+//             try {
+//                 console.log(JSON.parse(body))
+//                 resolve(JSON.parse(body));
+//             } catch (e) {
+//                 reject(e);
+//             }
+//         })
+//     })
+// }
+
+// getWorldData()
+
+
+
+
+// Name: Afghanistan { 
+//     "name": "Afghanistan", 
+// "topLevelDomain": [".af"], 
+// "alpha2Code": "AF", 
+// "alpha3Code": "AFG", 
+// "callingCodes": ["93"], 
+// "capital": "Kabul", 
+// "altSpellings": ["AF", "Afġānistān"], 
+// "region": "Asia", 
+// "subregion": "Southern Asia", 
+// "population": 27657145, 
+// "latlng": [33, 65], 
+// "demonym": "Afghan", 
+// "area": 652230, 
+// "gini": 27.8, 
+// "timezones": ["UTC+04:30"], 
+// "borders": ["IRN", "PAK", "TKM", "UZB", "TJK", "CHN"], 
+// "nativeName": "افغانستان", 
+// "numericCode": "004", 
+// "currencies": [{ "code": "AFN", "name": "Afghan afghani", "symbol": "؋" }],
+//  "languages": [
+//  { "iso639_1": "ps", "iso639_2": "pus", "name": "Pashto", "nativeName": "پښتو" }, 
+//  { "iso639_1": "uz", "iso639_2": "uzb", "name": "Uzbek", "nativeName": "Oʻzbek" }, 
+//  { "iso639_1": "tk", "iso639_2": "tuk", "name": "Turkmen", "nativeName": "Türkmen" }], 
+//  "translations": 
+//  { "de": "Afghanistan", "es": "Afganistán", "fr": "Afghanistan", "ja": "アフガニスタン", 
+//  "it": "Afghanistan", 
+//  "br": "Afeganistão", 
+//  "pt": "Afeganistão" 
+// }, 
+// "flag": "https://restcountries.eu/data/afg.svg", 
+// "regionalBlocs": [
+// { "acronym": "SAARC", "name": "South Asian Association for Regional Cooperation", "otherAcronyms": [], "otherNames": [] }
+// ] }
